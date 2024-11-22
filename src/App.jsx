02252c6523db1450a52fd0d18ce0989678caa977
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-23 01:31:10 +0530

    App.jsx Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Template1 from './components/Template1';
+import Template1Page from './pages/Template1';
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '352112236-q2che8r2qdoscgovbi09vkc7oglgkj8a.apps.googleusercontent.com';
 
@@ -12,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/template1" element={<Template1Page />} />
         </Routes>
       </BrowserRouter>
     </GoogleOAuthProvider>
