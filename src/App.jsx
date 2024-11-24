88ab8a45a397c27f6aa413dc2df50a168f3b7766
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-24 16:24:01 +0530

    Changes in Dashboard.jsx and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+// import Login from './pages/Login';
+// import Dashboard from './pages/Dashboard';
+// import Template1 from './components/Template1';
+// import SaveResume from './components/SaveResumeModal';
+
+// const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '352112236-q2che8r2qdoscgovbi09vkc7oglgkj8a.apps.googleusercontent.com';
+
+// function App() {
+//   return (
+//     <GoogleOAuthProvider clientId={googleClientId}>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Login />} />
+//           <Route path="/dashboard" element={<Dashboard />} />
+//           <Route path="/template1" element={<Template1 />} />
+//           <Route path="/myresume" element={<SaveResume />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </GoogleOAuthProvider>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
@@ -9,16 +35,15 @@ const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '352112236-q2che
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId={googleClientId}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/template1" element={<Template1 />} />
-          <Route path="/myresume" element={<SaveResume />} />
-        </Routes>
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+   <GoogleOAuthProvider clientId={googleClientId}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/template1" element={<Template1 />} />
+      </Routes>
+    </Router>
+   </GoogleOAuthProvider>
   );
 }
 
