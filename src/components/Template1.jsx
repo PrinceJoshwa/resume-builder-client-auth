2e1872357845a631c3f2fb4cab521a5a3fb4bd87
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-27 01:51:17 +0530

    Changes in Template1.jsx

--- src/components/Template1.jsx
+++ src/components/Template1.jsx
@@ -2054,6 +2054,12 @@ function Template1() {
             <Download size={20} />
             Download PDF
           </button>
+          <button
+              onClick={() => navigate('/')}
+              className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+            >
+              Back to Dashboard
+            </button>
         </div>
 
         {/* Save Modal */}
